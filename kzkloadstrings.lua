12340ce2ec326e70f7f39f3a75a237516c55da2e
ykknzo-hub <kenzielimonn@gmail.com>
2025-12-12 00:20:58 +0000

    Update and rename kzkloadstrings.txt to kzkloadstrings.lua

--- kzkloadstrings.lua
+++ kzkloadstrings.lua
@@ -5,4 +5,5 @@ loadstring(game:HttpGet("https://pastebin.com/raw/RfFGyXEX"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7yd7/Hub/refs/heads/Branch/GUIS/Emotes.lua"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/7kuNSTcS"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/v2NZtm4c"))()
+
 loadstring(game:HttpGet("https://pastebin.com/raw/RfFGyXEX"))()
